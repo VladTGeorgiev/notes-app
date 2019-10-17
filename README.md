0d399ee768ca54d90f7dd809736a27c00694497c
VladTGeorgiev <vlad.georgiev@outlook.com>
2019-10-17 20:53:58 +0100

    npm validator, chalk, nodemon

---
+++ README.md
@@ -0,0 +1,3 @@
+npm validator
+npm chalk
+npm nodemon
---
+++ app.js
@@ -0,0 +1,6 @@
+const chalk = require('chalk');
+const getNotes = require('./notes.js');
+
+const msg = getNotes();
+
+console.log(chalk.green.inverse(msg));

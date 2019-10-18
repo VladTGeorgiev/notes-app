9669b03cc1c617ad18ce8852f67ff626a45fd062
VladTGeorgiev <vlad.georgiev@outlook.com>
2019-10-18 22:54:38 +0100

    Exploring npm yargs

--- app.js
+++ app.js
@@ -1,6 +1,52 @@
 const chalk = require('chalk');
 const getNotes = require('./notes.js');
+const yargs = require('yargs');
 
-const msg = getNotes();
+yargs.version('1.1.0');
 
-console.log(chalk.green.inverse(msg));
+yargs.command({
+	command  : 'add',
+	describe : 'Add a note',
+	builder  : {
+		title : {
+			describe     : 'Note title',
+			demandOption : true,
+			type         : 'string'
+		},
+		body  : {
+			describe     : 'Note body',
+			demandOption : true,
+			type         : 'string'
+		}
+	},
+	handler  : function (argv) {
+		console.log('Title: ', argv.title);
+		console.log('Body: ', argv.body);
+	}
+});
+
+yargs.command({
+	command  : 'remove',
+	describe : 'Remove',
+	handler  : function () {
+		console.log('removing');
+	}
+});
+
+yargs.command({
+	command  : 'list',
+	describe : 'list',
+	handler  : function () {
+		console.log('listing all notes');
+	}
+});
+
+yargs.command({
+	command  : 'read',
+	describe : 'Read',
+	handler  : function () {
+		console.log('reading');
+	}
+});
+
+yargs.parse();
